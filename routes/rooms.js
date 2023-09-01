ef7e45d695008baecd6264194d1470fcf5b648e3
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2023-09-01 20:32:30 +0200

    Nitpick crud, example

--- routes/rooms.js
+++ routes/rooms.js
@@ -13,46 +13,58 @@ router.post('/rooms', async (req, res, next) => {
     }
 });
 
+// GET Rooms
+router.get('/rooms', async (req, res, next) => {
+    try {
+        const rooms = await Room.findAll();
+        res.json({ rooms });
+    } catch (error) {
+        next(error);
+    }
+});
+
+// PUT: Update an existing room by ID 📝 there is nothing to update in a room
+router.put('/rooms/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        res.json({ message: `Room with id:${id} updated successfully` });
+    } catch (error) {
+        // Handle errors
+        next(error);
+    }
+});
+
 // DELETE a room
 router.delete('/rooms', async (req, res, next) => {
     try {
-      const { id } = req.body;
-  
-      if (!id || typeof id !== 'number' || !Number.isInteger(id)) {
-        return res.status(422).json({ error: "Invalid room_id. It should be a whole number" });
-      }
-  
-      const deletedRoom = await Room.destroy({
-        where: {
-          id: id
+        const { id } = req.body;
+
+        if (!id || typeof id !== 'number' || !Number.isInteger(id)) {
+            return res.status(422).json({ error: "Invalid room_id. It should be a whole number" });
         }
-      });
-  
-      if (deletedRoom === 0) {
-        return res.status(404).json({ error: `Room with id:${id} not found` });
-      }
-  
-      res.json({ message: `Room with id:${id} was deleted` });
-    } catch (error) {
-      // Handle errors
-      next(error);
-    }
-  });
 
-  
-// Get Rooms
-router.get('/rooms', async (req, res, next) => {
-    try {
-      const rooms = await Room.findAll();
-      res.json({ rooms });
-    } catch (error) {
-      next(error);
-    }
-  });
+        const deletedRoom = await Room.destroy({
+            where: {
+                id: id
+            }
+        });
 
-  // PUT: Update a room
-router.put('/rooms', (req, res, next) => {
-  res.json({message: "Room updated"});
+        if (deletedRoom === 0) {
+            return res.status(404).json({ error: `Room with id:${id} not found` });
+        }
+
+        res.json({ message: `Room with id:${id} was deleted` });
+    } catch (error) {
+        // Handle errors
+        next(error);
+    }
 });
 
 module.exports = router;
+
+/* Examples on Postman
+DELETE
+{
+  "id": 1
+}
+*/
